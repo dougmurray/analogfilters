3593f507883fd5eebc0fccf6403de5f8ecda9904
Douglass Murray <godzilla@Douglasss-MBP.fios-router.home>
2018-02-21 19:19:53 -0500

    WIP. Addition of multiple feedback high pass filter script. Might be off by a factor of 2 for resistors.

---
+++ analogfilters/highpass/multiple_feedback_hp.py
@@ -0,0 +1,106 @@
+"""Multiple Feedback Low Pass Filter
+
+ Based on Figure 5-70 in Op Amp Applications Handbook, by Walt Jung.
+ Figure 5-70 ASCII style
+        +------+--------------+-----o
+        C4     R5             |
+        |      |   ___amp__   |
+ o--C1--+--C3--+--| -      |  |
+        |         |    out |--+
+        R2     +--| +      |
+        |      |  |________|
+       -_-    -_-
+"""
+import numpy as np
+
+def interface():
+    """Runs general interface/welcome screen
+    """
+    print("")
+    print("Multple Feedback Low Pass Filter")
+    print("--------------------------------")
+    print("         +------+--------------+-----o")
+    print("         C4     R5             |")
+    print("         |      |   ___amp__   |")
+    print(" o---C1--+--C3--+--| -      |  |")
+    print("         |         |    out |--+")
+    print("         R2     +--| +      |")
+    print("         |      |  |________|")
+    print("        -_-    -_-")
+    choice = int(input("Calculate filter response (1) or calculate component values (2): "))
+
+    if choice == 1:
+        COne = float(input("Input C1: "))
+        cThree = float(input("Input C3: "))
+        cFour = float(input("Input C4: "))
+        rTwo = float(input("Input R2 in uF: "))
+        rFive = float(input("Input R5 in uF: "))
+        component_transfer_function(COne, cThree, cFour, rTwo, rFive)
+    elif choice == 2:
+        cut_off_freq = float(input("Input cut-off frequency: "))
+        decent_cap = float(input("Input C1 capacitance in uF: "))
+        cut_off_function(cut_off_freq, decent_cap)
+    else:
+        print("Please choose either 1 or 2")
+
+def cut_off_function(f0, c1):
+    """Calculates the component values for the given cut-off frequency
+
+    Args:
+        f0: cut-off frequency
+        c1: based line capacitor
+    Returns:
+        None
+    """
+    # Intialized constants
+    alpha = 0.5 # damping ratio, default (1/2)
+    H = 1.0 # circuit gain at passband, default 1 (unity)
+
+    # Convert to uF
+    realC1 = c1 * 1.0e-6
+
+    k = (2.0 * np.pi * f0 * realC1)
+    c3 = c1
+    c4 = c1 / H
+    r2 = alpha / (k * (2 + (1 / H)))
+    r5 = (H * (2 + (1. / H))) / (alpha * k)
+
+    print("For cutoff frequency: ", f0, " Hz")
+    print("C3: ", c3)
+    print("C4: ", c4)
+    print("R2: ", r2)
+    print("R5: ", r5)
+    print("C1: ", realC1)
+
+def component_transfer_function(c1, c3, c4, r2, r5):
+    """Calculates the cut-off frequency (f0) and damping ratio (alpha) based
+    on filter's component choices
+
+    Args:
+        r2, r5: resistors in filter, Ohms
+        c1, c3, c4: capacitors in filter, uF
+    Returns:
+        None
+    """
+    # Convert to uF
+    realC1 = c1 * 1.0e-6
+    realC3 = c3 * 1.0e-6
+    realC5 = c5 * 1.0e-6
+
+
+    H = 1.0 # circuit gain at passband, defualt 1 (unity)
+    alpha = np.sqrt(H * r2 * 2.0 * (2.0 + (1.0 / H)) / r5) 
+    k = (H* (2.0 + (1.0/ H))) / (alpha * r5)
+    f0 = k / (2.0 * np.pi * c1)
+
+    print("For C1: ", realC1)
+    print("For C3: ", realC3)
+    print("For C4: ", realC4)
+    print("For R2: ", r2)
+    print("For R5: ", r5)
+    print("Cutoff frequency: ", f0, " Hz")
+    print("Damping ratio: ", alpha, " arb.")
+
+if __name__ == '__main__':
+    while(1):
+        interface()
